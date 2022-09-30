eee37174c986a2031b5b981093970bde22c5e3b4
danfa-peng <danfa_peng@apple.com>
2022-10-01 01:48:32 +0800

    add try catch

--- Week1/Day2/Fractions/src/FractionTest.java
+++ Week1/Day2/Fractions/src/FractionTest.java
@@ -5,10 +5,14 @@ class FractionTest {
 
     @Test
     void testDividedBy() {
-        Fraction fraction1 = new Fraction(1,0);
-        Fraction fraction2 = new Fraction(1,6);
+        try {
+            Fraction fraction1 = new Fraction(1, 0);
+            Fraction fraction2 = new Fraction(1, 6);
 
-        Fraction testResult = fraction1.dividedBy(fraction2);
-        Assertions.assertEquals(testResult.convertToString(), "-3/2");
+            Fraction testResult = fraction1.dividedBy(fraction2);
+            Assertions.assertEquals(testResult.toDouble(), 0);
+        } catch (NullPointerException e) {
+            System.out.println("exception msg" + e.getMessage());
+        }
     }
 }
